0ae53db2f2de8405666492900f5953e8fc1d6aa5
Gunther Brunner <gunta85@gmail.com>
2024-08-29 04:40:01 +0900

    First commit

---
+++ src/consts.ts
@@ -0,0 +1,24 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+import { type Multilingual } from "@/i18n";
+
+export const SITE_TITLE: string | Multilingual = {
+  en: "Translation Advancement Manifesto",
+  ja: "翻訳躍進マニフェスト",
+  "zh-cn": "翻译飞跃宣言"
+};
+
+export const SITE_DESCRIPTION: string | Multilingual = {
+  en: "Translation Advancement Manifesto - A New Economic Revolution for Japan",
+  ja: "翻訳躍進マニフェスト - 日本の新たな経済革命",
+  "zh-cn": "翻译飞跃宣言 - 日本新的经济革命"
+};
+
+export const X_ACCOUNT: string | Multilingual = "@psephopaiktes";
+
+export const NOT_TRANSLATED_CAUTION: string | Multilingual = {
+  en: "This page is not available in your language.",
+  ja: "このページはご利用の言語でご覧いただけません。",
+  "zh-cn": "此页面不支持您的语言。"
+};
---
+++ src/locales.ts
@@ -0,0 +1,25 @@
+// locale settings for this theme
+// https://astro-i18n-starter.pages.dev/setup/
+
+export const DEFAULT_LOCALE_SETTING: string = "ja";
+
+export const LOCALES_SETTING: LocaleSetting = {
+  "en": {
+    "label": "English"
+  },
+  "ja": {
+    "label": "日本語"
+  },
+  "zh-cn": {
+    "label": "简体中文",
+    "lang": "zh-CN"
+  }
+};
+
+interface LocaleSetting {
+  [key: Lowercase<string>]: {
+    label: string;
+    lang?: string;
+    dir?: 'rtl' | 'ltr';
+  };
+} // refer: https://starlight.astro.build/reference/configuration/#locales
